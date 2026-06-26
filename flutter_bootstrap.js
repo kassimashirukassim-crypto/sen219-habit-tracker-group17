8d2ec5c113bd1ed35003a7728d6e731bbe6436e0
kassimashirukassim-crypto <kassimashirukassim@gmail.com>
2026-06-26 13:23:07 +0000

    Deploying to gh-pages from @ kassimashirukassim-crypto/sen219-habit-tracker-group17@9907f57d91825d7bd72e049e57a7e5ebcbc8d63d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3262230755" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "758998084" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
